17d153e281c532614c85f0e25ecb72b75cceb052
not <62653093+notza6982@users.noreply.github.com>
2021-04-13 15:00:45 +0700

    update menu

--- Client/my-app/src/App.js
+++ Client/my-app/src/App.js
@@ -1,30 +1,88 @@
-import './App.css';
-import HeaderMenu from './components/layouts/HeaderMenu.js'
-import { BrowserRouter as Router, Route, Link,Switch} from 'react-router-dom';
-import AIhome from './components/pages/AI/AIhome.js'
-import Home from './components/pages/Home/Home'
-import ShopHome from './components/pages/Shop/ShopHome'
-import ContactusHome from './components/pages/Contactus/ContactusHome'
-import { Layout } from 'antd';
+import HeaderMenu from "./components/layouts/HeaderMenu.js";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import AIhome from "./components/pages/AI/AIhome.js";
+import Home from "./components/pages/Home/Home";
+import ShopHome from "./components/pages/Shop/ShopHome";
+import ContactusHome from "./components/pages/Contactus/ContactusHome";
+import { Layout, Row, Col, Avatar, Space, Menu, Dropdown } from "antd";
+import { Link } from "react-router-dom";
+import { UserOutlined } from "@ant-design/icons";
 const { Header, Footer, Sider, Content } = Layout;
+
+const menu = (
+  <Menu>
+    <Menu.Item key="0">เข้าสู่ระบบ</Menu.Item>
+    <Menu.Item key="1">ตั้งค่าบัญชีผู้ใช้งาน</Menu.Item>
+    <Menu.Divider />
+    <Menu.Item key="3">ออกจากระบบ</Menu.Item>
+  </Menu>
+);
+
 function App() {
   return (
-    <div >
-    <Layout>
-      <Header>
-      <HeaderMenu/>
-      </Header>
-      <Content>
-      <div>
-      <Route exact path="/GlassShop/AI" component={AIhome}/>
-      <Route exact path="/GlassShop/Home" component={Home}/>
-      <Route exact path="/GlassShop/Shopping" component={ShopHome}/>
-      <Route exact path="/GlassShop/Contact" component={ContactusHome}/>
-      </div>
-      </Content>
-      <Footer style={{textAlign:"center"}}>2021 © power by Glass Shop Group</Footer>
-    </Layout>
-      
+    <div>
+      <Layout>
+        <Header>
+          <Row style={{ justifyContent: "space-between" }}>
+            <Col style={{ color: "white" }}>
+              <Link to="/GlassShop/Home">
+                Glass Shop(หารูปแปะ คลิกแล้วไปหน้า Home)
+              </Link>
+            </Col>
+            <Col>
+              <Row>
+                <Space>
+                  <Col>
+                    <HeaderMenu />
+                  </Col>
+                  <Col>
+                    <Dropdown overlay={menu} trigger={["click"]}>
+                      <a onClick={(e) => e.preventDefault()}>
+                        <Avatar icon={<UserOutlined />} size={50} />
+                        <span
+                          style={{
+                            padding: 10,
+                            color: "white",
+                          }}
+                        >
+                          Test
+                        </span>
+                      </a>
+                    </Dropdown>
+                  </Col>
+                </Space>
+              </Row>
+            </Col>
+          </Row>
+        </Header>
+        <Content
+          style={{
+            margin: "0px 0px",
+            padding: 24,
+            minHeight: 490,
+            background: "white",
+            overflow: "hidden",
+          }}
+        >
+          <div>
+            <Route exact path="/">
+              <Redirect to="/GlassShop/Home" />
+            </Route>
+            <Route exact path="/GlassShop/AI" component={AIhome} />
+            <Route exact path="/GlassShop/Home" component={Home} />
+            <Route exact path="/GlassShop/Shopping" component={ShopHome} />
+            <Route exact path="/GlassShop/Contact" component={ContactusHome} />
+          </div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          2021 © power by Glass Shop Group
+        </Footer>
+      </Layout>
     </div>
   );
 }
--- Client/my-app/src/components/pages/Home/Home.js
+++ Client/my-app/src/components/pages/Home/Home.js
@@ -1,11 +1,64 @@
-import React from 'react'
+import React from "react";
+import { Row, Col, Card } from "antd";
 
 function Home() {
-    return (
-        <div>
-            dfgdfgfdg
-        </div>
-    )
+  return (
+    <div style={{ padding: 24 }}>
+      <Row gutter={[32, 32]}>
+        <Col span={24}>
+          <Card
+            style={{
+              marginRight: 10,
+              borderColor: "#888888",
+              borderWidth: 2,
+              boxShadow: "2px 2px 1px 1px #888888",
+              textAlign: "center",
+              fontSize: 20,
+              borderRadius: 20,
+            }}
+          >
+            Glass Shop
+          </Card>
+        </Col>
+      </Row>
+
+      <br />
+
+      <Row gutter={[32, 32]}>
+        <Col xs={24} xl={12}>
+          <Card
+            style={{
+              marginRight: 10,
+              borderColor: "#888888",
+              borderWidth: 2,
+              boxShadow: "2px 2px 1px 1px #888888",
+              textAlign: "center",
+              fontSize: 20,
+              borderRadius: 20,
+            }}
+          >
+            ตัวอย่างหน้าช่วยเลือกแว่น
+          </Card>
+        </Col>
+
+        <Col xs={24} xl={12}>
+          <Card
+            style={{
+              marginRight: 10,
+              borderColor: "#888888",
+              borderWidth: 2,
+              boxShadow: "2px 2px 1px 1px #888888",
+              textAlign: "center",
+              fontSize: 20,
+              borderRadius: 20,
+            }}
+          >
+            Promotion แว่น
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
