9e89b35f2c2e5d1dafec5c0955de2d7b9285b95d
16334 <s6004062616334@email.kmutnb.ac.th>
2021-04-15 23:20:59 +0700

    add Contactus

--- Client/my-app/src/components/pages/Contactus/ContactusHome.js
+++ Client/my-app/src/components/pages/Contactus/ContactusHome.js
@@ -1,9 +1,61 @@
 import React from 'react'
+import './ContactUs.css'
+import { Row, Col } from 'antd';
 
 function ContactusHome() {
     return (
-        <div>
-            ContactusHome
+
+        <div class="Con-container">
+            <h1 class="Con-text-head">Contact Us</h1>
+            <br></br>
+            <div>
+                <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+                    <Col className="gutter-row" span={6}>
+                        <div class="Con-styleimg">img</div>
+                    </Col>
+                    <Col className="gutter-row" span={6}>
+                        <div class="Con-styleimg">img</div>
+                    </Col>
+                    <Col className="gutter-row" span={6}>
+                        <div class="Con-styleimg">img</div>
+                    </Col>
+                    <Col className="gutter-row" span={6}>
+                        <div class="Con-styleimg">img</div>
+                    </Col>
+                </Row>
+                <br></br>
+                <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
+                    <Col className="gutter-row" span={6}>
+                        <div class="Con-styletext">
+                            <p>นางสาว ภาสินี เพชรสม</p>
+                            <p>6004062610093</p>
+                            <p>sec.1</p>
+                        </div>
+                    </Col>
+                    <Col className="gutter-row" span={6}>
+                        <div class="Con-styletext">
+                            <p>นาย นพกร อรพรตระกูล</p>
+                            <p>6004062620030</p>
+                            <p>sec.1</p>
+                        </div>
+                    </Col>
+                    <Col className="gutter-row" span={6}>
+                        <div class="Con-styletext">
+                            <p>นาย ภาคภูมิ ศรีเสน</p>
+                            <p> 6004062620048</p>
+                            <p>sec.1</p>
+                        </div>
+                    </Col>
+                    <Col className="gutter-row" span={6}>
+                        <div class="Con-styletext">
+                            <p>นางสาว ประวีณา ดวงจันทร์</p>
+                            <p>6004062616334</p>
+                            <p>sec.2</p>
+                        </div>
+                    </Col>
+                </Row>
+            </div>
+           
         </div>
     )
 }
