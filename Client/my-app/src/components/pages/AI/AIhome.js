91622c9fd6082e759c61deece013cf91acf18167
prkpwm <prkpwm.dev@gmail.com>
2021-04-18 16:22:00 +0700

    Edit ContactusHome

--- Client/my-app/src/components/pages/AI/AIhome.js
+++ Client/my-app/src/components/pages/AI/AIhome.js
@@ -2,11 +2,9 @@ import React from 'react'
 
 function AIhome() {
     return (
-        <div >
             <iframe width="1920" height="800"
                 src="http://localhost:8080/upload">
             </iframe>
-        </div>
     )
 }
 
--- Client/my-app/src/components/pages/Shop/Paging.js
+++ Client/my-app/src/components/pages/Shop/Paging.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Pagination } from 'antd';
 
 function Paging() {
+    
     return(<Pagination defaultCurrent={1} total={10} />)
 }
 export default Paging
