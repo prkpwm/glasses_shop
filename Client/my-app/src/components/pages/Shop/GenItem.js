4c35feffb228e1a598f147b232caa81ad691f770
Pasinee <pasinee.ch@gmail.com>
2021-04-21 20:33:14 +0700

    shop V1

--- Client/my-app/src/components/pages/Shop/GenItem.js
+++ Client/my-app/src/components/pages/Shop/GenItem.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import { Row, Col, Button, Card } from "antd";
+const { Meta } = Card;
 const style = { background: "#F8F9F9", padding: "8px 8px", height: "250px" };
 const fontRight = { textAlign: "right" };
 const blue = {
@@ -30,20 +31,15 @@ export default class PersonList extends React.Component {
           {
             this.state.datas.map(data =>
               <Col className="gutter-row" xs={24} md={12} xl={6}>
-                <div style={style} >
-                  <img
-                    src={data[3]}
-                    alt="glasses!!"
-                    width="95%"
-                    height="150"
-                  />
-                  <br />
-                  <a href="#">{data[1]}</a>
+                <Card
+                  hoverable
+                  cover={<img alt="glasses!!" src={data[3]} width="95%" height="150" />}>
+                  <Meta title={data[1]} description="www.instagram.com" />
                   <p style={fontRight}>{data[2]} ฿</p>
-                  <button type="button" style={blue}>
+                  <Button type="button" style={blue}>
                     Add to cart
-                  </button>
-                </div>
+                  </Button>
+                </Card>
               </Col>
             )
           }
--- Client/my-app/src/components/pages/Shop/GenItem_old.js
+++ Client/my-app/src/components/pages/Shop/GenItem_old.js
@@ -1,57 +1,55 @@
-import React, { useState, useEffect } from "react";
-import { Row, Col, Button, Card } from "antd";
+import React from 'react';
 import axios from 'axios';
+import { Row, Col, Button, Card } from "antd";
 const style = { background: "#F8F9F9", padding: "8px 8px", height: "250px" };
 const fontRight = { textAlign: "right" };
-
 const blue = {
   textAlign: "right",
   backgroundColor: "#330033",
   color: "#FFFFFF",
 };
 
-function componentDidMount() {
-  axios.get("http://localhost:8080/getinfo/iteminfo")
-    .then(res => {
-      const persons = res.data;
-      this.setState({ persons });
-    })
-}
+export default class PersonList extends React.Component {
+  state = {
+    datas: [[]]
+  }
 
-var testcard =<Col className="gutter-row" xs={24} md={12} xl={6}>
-<div style={style} >
-  <img
-    src="http://localhost:3000/img/dumpGlasses.png"
-    alt="glasses!!"
-    width="95%"
-    height="150"
-  />
-  <br />
-  <a href="#">Glasses</a>
-  <p style={fontRight}>1,000 ฿</p>
-  <button type="button" style={blue}>
-    Add to cart
-  </button>
-</div>
-</Col>
-function GenItem() {
-  const [allcard, setallcard] = useState([testcard]);
-  const addcard = () => {
-    let card = [...allcard];
-    card.push(testcard);
-    setallcard(card);
-    console.log(allcard);
-  };
-  return (
-    <div>
-      <Button type="primary" onClick={() => {addcard();}}>
-        Add 1 card
-      </Button>
-      <Row gutter={[16, 24]}>
-          {allcard}
-          </Row>
-    </div>
-  );
-}
+  componentDidMount() {
+    axios.get("/getinfo/iteminfo")
+      .then(res => {
+        const datas = res.data;
+        console.log(datas)
+        this.setState({ datas });
+      })
+  }
 
-export default GenItem;
+  render() {
+    return (
+      <div>
+        <Row gutter={[16, 24]}>
+          {
+            this.state.datas.map(data =>
+              <Col className="gutter-row" xs={24} md={12} xl={6}>
+                <div style={style} >
+                  <img
+                    src={data[3]}
+                    alt="glasses!!"
+                    width="95%"
+                    height="150"
+                  />
+                  <br />
+                  <a href="#">{data[1]}</a>
+                  <p style={fontRight}>{data[2]} ฿</p>
+                  <button type="button" style={blue}>
+                    Add to cart
+                  </button>
+                </div>
+              </Col>
+            )
+          }
+        </Row>
+      </div>
+
+    )
+  }
+}
