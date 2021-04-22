8c6e7483d4ed625191fad28ba2247333c4688b02
not <62653093+notza6982@users.noreply.github.com>
2021-04-22 09:28:20 +0700

    profile v 1.0.4

--- Client/my-app/src/components/pages/UserInfo/Profileinfo.js
+++ Client/my-app/src/components/pages/UserInfo/Profileinfo.js
@@ -1,5 +1,7 @@
-import React, { useState} from "react";
-import { Row, Col, Form, Input, Button } from "antd";
+import React, { useState } from "react";
+import { Row, Col, Form, Input, Button, Select,DatePicker } from "antd";
+var dayjs = require('dayjs')
+const { Option } = Select;
 
 function Profileinfo() {
     const [editform, seteditform] = useState(true);
@@ -12,40 +14,63 @@ function Profileinfo() {
     return (
         <div>
             <Form
-                            name="basic"
-                            initialValues={{ Name: "สมชาย", Surname: "อยู่ดี" }}
-                            onFinish={onFinish}
-                            onFinishFailed={onFinishFailed}
+                name="basic"
+                initialValues={{ Name: "สมชาย", Surname: "อยู่ดี",Birthday:dayjs(new Date()),Gender:"Male" }}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+            >
+                <Row>
+                    <Col span={11}>
+                        <Form.Item
+                            label="ชื่อ"
+                            name="Name"
                         >
-                            <Row>
-                                <Col span={11}>
-                                    <Form.Item
-                                        label="ชื่อ"
-                                        name="Name"
-                                    >
-                                        <Input disabled={editform} />
-                                    </Form.Item>
-                                </Col>
-                                <Col span={1} />
-                                <Col span={11}>
-                                    <Form.Item
-                                        label="นามสกุล"
-                                        name="Surname"
-                                    >
-                                        <Input disabled={editform} />
-                                    </Form.Item>
-                                </Col>
+                            <Input disabled={editform} />
+                        </Form.Item>
+                    </Col>
+                    <Col span={1} />
+                    <Col span={11}>
+                        <Form.Item
+                            label="นามสกุล"
+                            name="Surname"
+                        >
+                            <Input disabled={editform} />
+                        </Form.Item>
+                    </Col>
+                </Row>
 
-                                <Form.Item >
-                                    <Button type={editform?"dashed":"danger"} onClick={() =>{seteditform(!editform)}}>
-                                    {editform?"Edit":"Cancel"}
-                                     </Button>
-                                    <Button type="primary" htmlType="submit">
-                                        Submit
+                <Row>
+                    <Col span={11}>
+                        <Form.Item
+                            label="วันเกิด"
+                            name="Birthday"
+                        >
+                            <DatePicker disabled={editform} format={'DD/MM/YYYY'} />
+                        </Form.Item>
+                    </Col>
+                    <Col span={1} />
+                    <Col span={11}>
+                        <Form.Item
+                            label="เพศ"
+                            name="Gender"
+                        >
+                            <Select style={{ width: 120 }} disabled={editform} >
+                                <Option value="Male">Male</Option>
+                                <Option value="Female">Female</Option>
+                            </Select>
+                        </Form.Item>
+                    </Col>
+                </Row>
+
+                <Form.Item >
+                    <Button type={editform ? "dashed" : "danger"} onClick={() => { seteditform(!editform) }}>
+                        {editform ? "Edit" : "Cancel"}
+                    </Button>
+                    <Button type="primary" htmlType="submit">
+                        Submit
                                     </Button>
-                                </Form.Item>
-                            </Row>
-                        </Form>
+                </Form.Item>
+            </Form>
         </div>
     )
 }
