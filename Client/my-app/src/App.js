348cd2afd525e45225685632c06d495d39b1e195
not <62653093+notza6982@users.noreply.github.com>
2021-04-26 11:24:33 +0700

    update set responsive size

--- Client/my-app/src/App.js
+++ Client/my-app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import HeaderMenu from "./components/layouts/HeaderMenu.js";
 import {
   BrowserRouter as Router,
@@ -44,131 +44,141 @@ var id = null
 function App() {
   const screens = useBreakpoint();
 
-  useEffect( () => {
+  useEffect(() => {
     if (localStorage.getItem('isLogin') == "true") {
-        id = localStorage.getItem('uid')
+      id = localStorage.getItem('uid')
     }
-    else if (localStorage.getItem('isLogin') == "false"){
-        id = sessionStorage.getItem('uid')
+    else if (localStorage.getItem('isLogin') == "false") {
+      id = sessionStorage.getItem('uid')
     }
-    if(id!=null){
+    if (id != null) {
       axios.get("/getinfobyid/userinfo/id/" + id)
         .then(res => {
-            const data = res.data[0];
-            console.log(data)
-            sessionStorage.setItem('nameshow',data[3])
+          const data = res.data[0];
+          console.log(data)
+          sessionStorage.setItem('nameshow', data[3])
         })
     }
-    
+
 
   }, [])
   return (
     <div>
-      {Object.entries(screens)
-        .filter(screen => !!screen[1])
-        .map(screen => { countcreen = screen[0] })}
-      <Layout>
-        <Header style={{ backgroundColor: "#EAECEE" }}>
-          <Row style={{ justifyContent: "space-between" }}>
-            <Col>
-              <Link to="/GlassesShop/Home">
-                <img
-                  src="http://localhost:3000/logo8.png"
-                  alt="glasses!!"
-                  width={countcreen != "xs" ? "160" : "100"}
-                  height="auto"
-                />
-              </Link>
-            </Col>
-            <Col>
-              <Row>
-                <Space>
-                  <Col>
-                    <HeaderMenu />
-                  </Col>
-                  <Col>
-                    <Space>
-                      <Link to="/GlassesShop/Mycart">
-                        <Avatar shape="square" style={{ color: "white", backgroundColor: "transparent" }}
-                          icon={< ShoppingCartOutlined style={{ fontSize: countcreen != "xs" ? 30 : 20, Align: "justify", color: "#424949" }} />} size={countcreen != "xs" ? 50 : 40} />
-                      </Link>
+      <Row gutter={{
+        // xs: '480px',
+        // sm: '767px',
+        // md: '1023px',
+        // lg: '1200px',
+        // xl: '1600px',
+        // xxl: '2000px',
+      }}>
+        {Object.entries(screens)
+          .filter(screen => !!screen[1])
+          .map(screen => { countcreen = screen[0] })}
+        <Layout>
+          <Header style={{ backgroundColor: "#EAECEE" }}>
+            <Row style={{ justifyContent: "space-between" }}>
+              <Col>
+                <Link to="/GlassesShop/Home">
+                  <img
+                    src="http://localhost:3000/logo8.png"
+                    alt="glasses!!"
+                    width={countcreen != "xs" ? "160" : "100"}
+                    height="auto"
+                  />
+                </Link>
+              </Col>
+              <Col>
+                <Row>
+                  <Space>
+                    <Col>
+                      <HeaderMenu />
+                    </Col>
+                    <Col>
+                      <Space>
+                        {(countcreen != "xss") &&
+                          <Link to="/GlassesShop/Mycart">
+                            <Avatar shape="square" style={{ color: "white", backgroundColor: "transparent" }}
+                              icon={< ShoppingCartOutlined style={{ fontSize: countcreen != "xs" ? 30 : 20, Align: "justify", color: "#424949" }} />} size={countcreen != "xs" ? 50 : 40} />
+                          </Link>
+                        }
+                        <Dropdown overlay={localStorage.getItem('userdata') || sessionStorage.getItem('userdata')
+                          ? menu1 : menu} trigger={["click"]}
+                        >
 
-                      <Dropdown overlay={localStorage.getItem('userdata') || sessionStorage.getItem('userdata')
-                        ? menu1 : menu} trigger={["click"]}
-                      >
+                          <a onClick={(e) => e.preventDefault()}>
+                            {localStorage.getItem('userdata') || sessionStorage.getItem('userdata') ?
+                              <Avatar src={"/loadimages/" + localStorage.getItem('path')} size={40} /> :
+                              <Avatar icon={<UserOutlined size={40} />} />
+                            }
+                            {(countcreen != "xs" && countcreen != "sm" && countcreen != "md") &&
+                              <span
+                                style={{
+                                  padding: 10,
+                                  color: "#424949",
+                                }}
+                              >
+                                {sessionStorage.getItem('nameshow')}
+                              </span>}
+                          </a>
+                        </Dropdown>
+                      </Space>
+                    </Col>
+                  </Space>
+                </Row>
+              </Col>
+            </Row>
+          </Header>
+          <Content
+            style={{
+              margin: "0px 0px",
+              padding: 24,
+              minHeight: 490,
+              background: "white",
+              overflow: "hidden",
+            }}
+          >
+            <div>
+              <Route exact path="/">
+                <Redirect to="/GlassesShop/Home" />
+              </Route>
+              <Route exact path="/GlassesShop/Register" component={RegisterHome} />
 
-                        <a onClick={(e) => e.preventDefault()}>
-                          {localStorage.getItem('userdata') || sessionStorage.getItem('userdata') ?
-                            <Avatar src={"/loadimages/" + localStorage.getItem('path')} size={40} /> :
-                            <Avatar icon={<UserOutlined size={40} />} />
-                          }
-                          {(countcreen != "xs"&&countcreen != "sm"&&countcreen != "md") &&
-                            <span
-                              style={{
-                                padding: 10,
-                                color: "#424949",
-                              }}
-                            >
-                              { sessionStorage.getItem('nameshow') }
-                            </span>}
-                        </a>
-                      </Dropdown>
-                    </Space>
-                  </Col>
-                </Space>
-              </Row>
-            </Col>
-          </Row>
-        </Header>
-        <Content
-          style={{
-            margin: "0px 0px",
-            padding: 24,
-            minHeight: 490,
-            background: "white",
-            overflow: "hidden",
-          }}
-        >
-          <div>
-            <Route exact path="/">
-              <Redirect to="/GlassesShop/Home" />
-            </Route>
-            <Route exact path="/GlassesShop/Register" component={RegisterHome} />
-            
-            <Route exact path="/GlassesShop/AI" component={AIhome} >
-              { (localStorage.getItem('uid') == null) ? <Redirect to="/GlassesShop/Login" /> : null}
-            </Route>
-            <Route exact path="/GlassesShop/Home" component={Home} />
-            <Route exact path="/GlassesShop/Shopping" component={ShopHome} >
-              { (localStorage.getItem('uid') == null) ? <Redirect to="/GlassesShop/Login" /> : null}
-            </Route>
-            <Route exact path="/GlassesShop/Contact" component={ContactusHome} >
-              {(localStorage.getItem('uid') == null) ? <Redirect to="/GlassesShop/Login" /> : null}
-            </Route>
-            <Route exact path="/GlassesShop/Mycart" component={Mycarthome} >
-              { (localStorage.getItem('uid') == null) ? <Redirect to="/GlassesShop/Login" /> : null}
-            </Route>
-            <Route exact path="/GlassesShop/Profile" component={Profile} >
-              { (localStorage.getItem('uid') == null) ? <Redirect to="/GlassesShop/Login" /> : null}
-            </Route>
-            <Route exact path="/GlassesShop/Pay" component={Pay} >
-              { (localStorage.getItem('uid') == null) ? <Redirect to="/GlassesShop/Login" /> : null}
-            </Route>
-            <Route exact path="/GlassesShop/Analysis" component={AnalysisHome} >
-              { (localStorage.getItem('uid') == null) ? <Redirect to="/GlassesShop/Login" /> : localStorage.getItem('role') != 1 ? <Redirect to="/GlassesShop/Login" /> : null}
-            </Route>
-          </div>
-        </Content>
-        <footer style={{
-          textAlign: "center",
-          padding: " 24px 50px",
-          color: "rgba(0, 0, 0, 0.85)",
-          fontSize: 14,
-          backgroundColor: "#f0f2f5"
-        }}>
-          2021 © power by Glasses Shop Group
+              <Route exact path="/GlassesShop/AI" component={AIhome} >
+                {(localStorage.getItem('uid') == null) ? <Redirect to="/GlassesShop/Login" /> : null}
+              </Route>
+              <Route exact path="/GlassesShop/Home" component={Home} />
+              <Route exact path="/GlassesShop/Shopping" component={ShopHome} >
+                {(localStorage.getItem('uid') == null) ? <Redirect to="/GlassesShop/Login" /> : null}
+              </Route>
+              <Route exact path="/GlassesShop/Contact" component={ContactusHome} >
+                {(localStorage.getItem('uid') == null) ? <Redirect to="/GlassesShop/Login" /> : null}
+              </Route>
+              <Route exact path="/GlassesShop/Mycart" component={Mycarthome} >
+                {(localStorage.getItem('uid') == null) ? <Redirect to="/GlassesShop/Login" /> : null}
+              </Route>
+              <Route exact path="/GlassesShop/Profile" component={Profile} >
+                {(localStorage.getItem('uid') == null) ? <Redirect to="/GlassesShop/Login" /> : null}
+              </Route>
+              <Route exact path="/GlassesShop/Pay" component={Pay} >
+                {(localStorage.getItem('uid') == null) ? <Redirect to="/GlassesShop/Login" /> : null}
+              </Route>
+              <Route exact path="/GlassesShop/Analysis" component={AnalysisHome} >
+                {(localStorage.getItem('uid') == null) ? <Redirect to="/GlassesShop/Login" /> : localStorage.getItem('role') != 1 ? <Redirect to="/GlassesShop/Login" /> : null}
+              </Route>
+            </div>
+          </Content>
+          <footer style={{
+            textAlign: "center",
+            padding: " 24px 50px",
+            color: "rgba(0, 0, 0, 0.85)",
+            fontSize: 14,
+            backgroundColor: "#f0f2f5"
+          }}>
+            2021 © power by Glasses Shop Group
         </footer>
-      </Layout>
+        </Layout>
+      </Row>
     </div>
   );
 }
