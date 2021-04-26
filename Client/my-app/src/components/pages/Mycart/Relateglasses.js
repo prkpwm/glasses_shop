bb7cdc2502f4f07955d236b9c8ad5c554359a7b3
not <62653093+notza6982@users.noreply.github.com>
2021-04-26 11:58:10 +0700

    update mycart v 1.0.3

--- Client/my-app/src/components/pages/Mycart/Relateglasses.js
+++ Client/my-app/src/components/pages/Mycart/Relateglasses.js
@@ -10,9 +10,15 @@ const blue = {
     backgroundColor: "#f2f4f4",
     color: "#000000",
 };
+const blueright = {
+    backgroundColor: "#f2f4f4",
+    color: "#000000",
+    float: 'right',
+};
 
 function Relateglasses() {
     const [datapopulate, setdatapopulate] = useState([[]]);
+    const [datapopup, setdatapopup] = useState([[]]);
     const [visible, setVisible] = useState(false);
 
     useEffect(() => {
@@ -24,14 +30,40 @@ function Relateglasses() {
             })
     }, [])
 
-    function getitem(id) {
-        // axios.get("/getinfobyid/iteminfo/GID/"+id+"")
-        //     .then(res => {
-        //         const datas = res.data;
-        //         console.log(datas)
-        //         setdatas(datas)
-        //     })
+    async function getitem(id) {
+        console.log(id)
+        await axios.get("/getinfobyid/iteminfo/GID/" + id + "")
+            .then(res => {
+                // const datas = res.data[0];
+                const datas = res.data;
+                console.log(datas)
+                setdatapopup(datas)
+            })
+
+        let body = {
+            iid: id,
+            uid: localStorage.getItem('uid'),
+        };
+        let message = ""
+        axios.get('/insert_statistics/', { params: { body } })
+            .then(response => {
+                message = response.data
+                console.log("response: ", response)
+
+            })
+            .catch(err => console.log(err));
+        if (message == "Success") {
+
+        }
+        else {
+            // return notification["error"]({
+            //   message: 'something wrong',
+            // });
+        }
         setVisible(true)
+    }
+    const onClickHandler = (data) => {
+
     }
 
     return (
@@ -41,19 +73,19 @@ function Relateglasses() {
                 {
                     datapopulate.map(data =>
                         <Col xs={24} lg={8} xl={8}>
+                            {console.log(data)}
                             <Card type="inner"
                                 hoverable
-                                cover={<img alt="glasses!!" onClick={() => setVisible(true)} src={data[7]} width="95%" height="150" />}
+                                cover={<img alt="glasses!!" onClick={() => getitem(data[2])} src={data[7]} width="95%" height="150" />}
                             >
-                                <div onClick={() => getitem(data[0])}>
-                                    <Meta title={data[5]} description={"Category : " + data[12]} /><br />
-                                    <p><h7 style={fontLeft}>{data[10]} </h7> <h7 style={fontRight}>{data[6]} ฿</h7></p>
+                                <div onClick={() => getitem(data[2])}>
+                                    <Meta title={data[5]} description={"Category : " + data[8]} /><br />
+                                    <p><h7 style={fontLeft}>{data[9]} </h7> <h7 style={fontRight}>{data[6]} ฿</h7></p>
                                     <br />
                                 </div>
-                                <Button type="button" onClick={() => console.log('Button')} style={blue} >
+                                <Button type="button" onClick={() => onClickHandler(data[2])} style={blue} >
                                     Add to cart
-                                </Button>
-
+                  </Button>
                             </Card>
                         </Col>
                     )
@@ -68,12 +100,30 @@ function Relateglasses() {
                 visible={visible}
                 onOk={() => setVisible(false)}
                 onCancel={() => setVisible(false)}
-                width="60%"
+                width="50%"
                 footer={null}
             >
-                <p>some contents...</p>
-                <p>some contents...</p>
-                <p>some contents...</p>
+                {datapopup.map(data => (
+                    < div style={{ padding: '20px 20px' }} >
+                        <div id='img' style={{textAlign:"center"}}>
+                            <img alt="glasses!!" src={data[3]} width="50%" height="150" />
+                        </div>
+                        <div>
+                            <h3 >{data[1]} </h3>
+                            <p style={{ float: 'right' }}>Category : {data[4]} </p>
+                            <p >{data[5]}</p>
+                            <p> Glasses are glasses .
+                  The type of glasses is {data[4]} .
+                  Just wear it on your head.
+                  The whole world will be beautiful immediately </p>
+                            <h4 style={{ textAlign: 'right' }}>{data[2]} ฿</h4>
+                            <p style={{ textAlign: 'right' }}> AVAILABILITY: <b>IN STOCK </b></p>
+                            <Button type="button" onClick={() => onClickHandler(data[0])} style={blueright}>
+                                Add to cart
+                   </Button>
+                        </div>
+                    </div>
+                ))}
             </Modal>
         </div>
     )
