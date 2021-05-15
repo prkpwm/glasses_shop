d64af79c93a6b14c2d2444b46442856be28702c0
16334 <s6004062616334@email.kmutnb.ac.th>
2021-05-15 13:35:37 +0700

    update

--- Client/my-app/src/components/pages/Analysis/AnalysisHome.js
+++ Client/my-app/src/components/pages/Analysis/AnalysisHome.js
@@ -2,6 +2,12 @@ import React, { useState } from "react";
 import { Row, Col, Card, Avatar, Divider, Button, Modal } from "antd";
 import PieCharted from './PieCharted.js';
 import BarCharted from "./BarCharted.js";
+const textname = {
+    textAlign: "center",
+    fontWeight: "bold",
+    fontSize: "18px",
+};
+
 function Analysis() {
 
     return (
@@ -9,16 +15,18 @@ function Analysis() {
             
             <div>
                 <Row>
-                    <Col span={8}><PieCharted /></Col>
-                    <Col span={8}><PieCharted /></Col>
-                    <Col span={8}><PieCharted /></Col>
+                    <Col span={8} xs={24} md={24}  xl={8} style={textname}>จำนวนยอดขายของแว่นแต่ละประเภท<PieCharted /></Col>
+                    <Col span={8} xs={24} md={24}  xl={8} style={textname}>จำนวนยอดคนสนใจของแว่นแต่ละประเภท<PieCharted /></Col>
+                    <Col span={8} xs={24} md={24}  xl={8} style={textname}>ช่วงเวลาที่คนเข้าชมสินคัา<PieCharted /></Col>
                 </Row>
             </div>
+            <br></br>
             <div>
                 <Row>
-                    <Col span={8}></Col>
-                    <Col span={8}><PieCharted /></Col>
-                    <Col span={8}></Col>
+                    <Col span={8} xl={4}></Col>
+                    <Col span={8} xs={24} md={24}  xl={8} style={textname}>ช่วงอายุสนใจของแว่นแต่ละประเภท<PieCharted /></Col>
+                    <Col span={8} xs={24} md={24}  xl={8} style={textname}>เพศสนใจของแว่นแต่ละประเภท<PieCharted /></Col>
+                    <Col span={8} xl={4}></Col>
                 </Row>
             </div>
         </div>
